507441b87a1b321ef9e3913573ca8c3137ba060c
kitzune-no-aki <github2@go-nihon.com>
2022-02-16 16:23:41 +0100

    Pokemonnumber

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' .png
 
 for(let i=1; i <= 151; i++){
-const newI = document.createElement('Img');
-newI.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${[i]}.png`
-const images = document.querySelector('#container');
-images.appendChild(newI);
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon')
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newI = document.createElement('Img');
+    newI.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${[i]}.png`
+    pokemon.appendChild(newI);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
 }
